71b774cfcd66cce2286414d379686eeb05992b36
Zackary <zspink@nea.k12.ar.us>
2021-12-02 11:57:19 -0600

    Added Shapes and Movement

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from shapes import Block
+from shapes import Column, Left_L, Right_L, T, Square
 
 def main():
     pg.init()
@@ -13,17 +13,30 @@ def main():
     column_padding = (screen.get_height() - rows * grid_size) // 2
 
     game_over = False
-    block = Block(5 * grid_size + row_padding, 7 * grid_size + column_padding, 4, grid_size)
+    block = Square(0 + row_padding, 0 + column_padding, grid_size)
+    clock = pg.time.Clock()
+    fps = 5
 
     while not game_over:
+        clock.tick(fps)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 game_over = True
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                if not block.colliding_with_left_side(row_padding):
+                    block.move_horizontal(-1 * grid_size)
+            if event.key == pg.K_RIGHT:
+                if not block.colliding_with_right_side(screen.get_width() - row_padding):
+                    block.move_horizontal(grid_size)
         
         screen.fill((0, 0, 0))
 
         draw_grid(rows, columns, screen, grid_size, row_padding, column_padding)
         block.draw_shape(screen)
+        if not block.colliding_with_bottom(screen.get_height() - column_padding):
+            block.move_down(grid_size)
 
         pg.display.update()
 
